e3a5b9df83bc2e01e28e716c771e16e4aa8eb207
dulchik <bubnyukab@gmail.com>
2026-02-25 15:48:30 +0100

    basic lexer

--- token/token.go
+++ token/token.go
@@ -1,6 +1,6 @@
 package token
 
-type TokeType string
+type TokenType string
 
 type Token struct {
 	Type	TokenType
@@ -32,3 +32,15 @@ const (
 	FUNCTION = "FUNCTION"
 	LET 	 = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn":	FUNCTION,
+	"let":	LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
