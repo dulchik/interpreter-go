535c6cf60490cda85815fd1d2392a076176c8c5a
dulchik <bubnyukab@gmail.com>
2026-02-27 18:04:20 +0100

    partially worked on parser

---
+++ parser/parser.go
@@ -0,0 +1,59 @@
+package parser
+
+import (
+	"github.com/dulchik/interpreter-go/ast"
+	"github.com/dulchik/interpreter-go/lexer"
+	"github.com/dulchik/interpreter-go/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l} 
+
+	// Read two tokens, so curToken and peekToken are both set
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default: 
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt  := &ast.LetStatement{Token: p.curToken}
+
+
+}
